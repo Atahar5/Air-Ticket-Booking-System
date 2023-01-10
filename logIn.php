09a31ee0338ed5d11c7e8fdc5190b13ac9ac9359
Atahar Istiaque <122311044+Atahar5@users.noreply.github.com>
2023-01-11 02:12:58 +0600

    Add files via upload

---
+++ logIn.php
@@ -0,0 +1,58 @@
+<?php
+require_once('DBconnect.php');
+session_start();
+
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['pass'] = $_POST['pass'];
+
+if(isset($_POST['email']) && isset($_POST['pass'])){
+    $e = $_POST['email'];
+    $p = $_POST['pass'];
+    $sql = "SELECT * from user WHERE email = '$e' ";
+    //Execute
+    $result = mysqli_query($conn, $sql);
+
+    //check if it returns an empty set
+    if(mysqli_num_rows($result) != 0){                          #check if the row exists
+        if($e == "admin47@gmail.com"){
+        header("Location: dashboard_admin.php");
+        }else{
+            header("Location: dashboard_user.php");
+        }
+    }else{
+        // echo "Username and Password is wrong";
+        header("Location: sign_in.php");
+    }
+}
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
